f3293afeef5ca6431e73fb92cfa04c42622143f5
Hugo Salins <anguyapy@gmail.com>
2020-07-11 21:27:45 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+                                        
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sistema de Ticket</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="App de Ticket para Coodeñe">
+    <meta name="author" content="@">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">      
+    <link href="css/bootstrap.min.css" rel="stylesheet">      
+
+    <!-- Estilos Propios -->
+    <link href="css_propio/normalize.css" rel="stylesheet">
+    <link href="css_propio/styles.css" rel="stylesheet">
+    
+    <!-- Icono de la App -->                                          
+    <link rel="shortcut icon" href="img/ico/coop.ico">
+
+    <!-- Font Awesome -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+             
+</head>
+        
