8a406037c43d00a5b9d70a0c7df0eaeb753ab059
Hugo Salins <anguyapy@gmail.com>
2020-07-11 21:30:10 -0400

    se actualizo el autor del header
    el autor se actualizo

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <title>Sistema de Ticket</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="App de Ticket para Coodeñe">
-    <meta name="author" content="@">
+    <meta name="author" content="@hugo">
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.css" rel="stylesheet">      
