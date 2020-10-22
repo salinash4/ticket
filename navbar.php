cd3a47218a1360b9f66026e129e2129b743f638a
Hugo Salins <anguyapy@gmail.com>
2020-10-22 18:45:53 -0300

    Se creo el archivo del menu de la app
    Se creo el archivo menu.php que contendra el contenido del menu de la app

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="es">
+
+    <!-- Definición del menú  NAVBAR DE LA APP-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a class="navbar-brand" href="dashboard.php"> HelpDesk </a>
+        <div class="collapse navbar-collapse" id="miNavbar">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu 3</a>
+                </li>
+            </ul>
+        </div>        
+    </nav>
+    <!-- Termina la definición del menú -->
+    <main role="main" class="container">
